66a8ee7a69a239d583b67b7040f93b0f0fe96c32
samitgoddamnit <joshua.awal@gmail.com>
2024-10-06 18:37:54 +0900

    started the academic feature. subject class should be working. Need to finish off the subject registration process and make sure it works.

--- academia.java
+++ academia.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+import java.util.HashMap;
 public class academia {
     
+    private HashMap<String,Subject> registry;
+
+    
+    
+    
+    private void registerSubject(){
+        
+    }
+    
+    
+    private Subject newSubject(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter subject name");
+        String n = input.nextLine();
+        System.out.println("Enter subject credit count");
+        int x = input.nextInt();
+        System.out.println("Enter subject teacher name");
+        String tn = input.nextLine();
+        System.out.println("Enter teacher's email");
+        String te = input.nextLine();
+        Subject s = new Subject(n,x,tn,te);
+        return s;
+    }
+
 }
--- subject.java
+++ subject.java
@@ -1,3 +1,42 @@
-public class subject {
-    
+public class Subject {
+    private String name;
+    private int creditValue;
+    private String teacherName;
+    private String teacherEmail;
+
+    public Subject(String name,int creditValue, String teacherName, String teacherEmail){
+        this.name = name;
+        this.creditValue  = creditValue;
+        this.teacherName = teacherName;
+        this.teacherEmail = teacherEmail;
+    }
+
+    public String name() {
+        return name;
+    }
+    public int creditValue() {
+        return creditValue;
+    }
+    public String teacherName() {
+        return teacherName;
+    }
+    public String teacherEmail() {
+        return teacherEmail;
+    }
+
+    public void sname(String name) {
+        this.name = name;
+    }
+
+    public void screditValue(int creditValue) {
+        this.creditValue = creditValue;
+    }
+
+    public void steacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public void steacherEmail(String teacherEmail) {
+        this.teacherEmail = teacherEmail;
+    }
 }
